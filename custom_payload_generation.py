ab15ca533aa0b7479a1d79c7f7e19338def5f475
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-15 20:50:23 +0000

    Create custom_payload_generation.py

---
+++ custom_payload_generation.py
@@ -0,0 +1,8 @@
+def generate_custom_payloads(api_documentation):
+    # Improved: Generate custom payloads based on API documentation or known attack patterns
+    custom_payloads = []
+
+    # Add your logic here to generate custom payloads
+    # Example: custom_payloads.append("<script>alert('XSS')</script>")
+
+    return custom_payloads
